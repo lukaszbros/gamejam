50665007a9c062d138044149c042208c72f5df5e
lukaszbros <lukasz.bros@gmail.com>
2013-11-17 13:14:56 +0100

    Player A and B sites

--- Site/js/player.js
+++ Site/js/player.js
@@ -1,17 +1,19 @@
-var socket = io.connect('http://racinglife.net:8090');
-socket.emit('player_connected',{player:'A'});
-function devOrientHandler(eventData) {
-	giro = new Array();
-	giro['alpha'] = eventData.alpha;
-    giro['beta'] = eventData.beta;
-    giro['gamma'] = eventData.gamma;
-    console.log(eventData);
-  	socket.emit('player_move',giro);
-}
-        
-if (window.DeviceOrientationEvent) {
-	console.log('DeviceOrientation is supported');
-    window.addEventListener('deviceorientation', devOrientHandler, false);
-} else {
-    console.log('Error! DeviceOrientation not supported');
+$(document).ready(function() {
+	var socket = io.connect('http://racinglife.net:8090');
+	socket.emit('player_connected',{player:player});
+	function devOrientHandler(eventData) {
+		giro = new Array();
+		giro['alpha'] = eventData.alpha;
+	    giro['beta'] = eventData.beta;
+	    giro['gamma'] = eventData.gamma;
+	    console.log(eventData);
+	  	socket.emit('player_move',giro);
+	}
+	        
+	if (window.DeviceOrientationEvent) {
+		console.log('DeviceOrientation is supported');
+	    window.addEventListener('deviceorientation', devOrientHandler, false);
+	} else {
+	    console.log('Error! DeviceOrientation not supported');
+	}
 }
