89360736b1cfb8636d123cd45ccf31deeedb1b15
lukaszbros <lukasz.bros@gmail.com>
2013-11-17 12:58:30 +0100

    Movement tracking added

--- Site/js/player.js
+++ Site/js/player.js
@@ -1,2 +1,12 @@
-var socket = io.connect('http://localhost:8090');
+var socket = io.connect('http://racinglife.net:8090');
 socket.emit('player_connected',{player:'A'});
+function devOrientHandler(eventData) {
+  socket.emit('player_move',eventData)
+}
+        
+if (window.DeviceOrientationEvent) {
+	console.log('DeviceOrientation is supported');
+    window.addEventListener('deviceorientation', devOrientHandler, false);
+} else {
+    console.log('Error! DeviceOrientation not supported');
+}
