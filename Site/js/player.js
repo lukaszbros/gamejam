2d689e3a96dcc16ef48fbd73db6745e6d22b3cee
lukaszbros <lukasz.bros@gmail.com>
2013-11-17 13:07:23 +0100

    Corrected player connect

--- Site/js/player.js
+++ Site/js/player.js
@@ -1,7 +1,12 @@
 var socket = io.connect('http://racinglife.net:8090');
 socket.emit('player_connected',{player:'A'});
 function devOrientHandler(eventData) {
-  socket.emit('player_move',eventData)
+	giro = new Array();
+	giro['alpha'] = eventData.alpha;
+    giro['beta'] = eventData.beta;
+    giro['gamma'] = eventData.gamma;
+    console.log(eventData);
+  	socket.emit('player_move',giro);
 }
         
 if (window.DeviceOrientationEvent) {
