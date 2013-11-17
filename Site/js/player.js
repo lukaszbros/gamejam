528a14772b4f08e4c7a356def19b5422deb57d48
lukaszbros <lukasz.bros@gmail.com>
2013-11-17 13:26:26 +0100

    Player B corrected

--- Site/js/player.js
+++ Site/js/player.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
+	var mainLoop;
+	
+	var alpha;
+    var beta;
+    var gamma;
+	
 	var socket = io.connect('http://racinglife.net:8090');
 	socket.emit('player_connected',{player:player});
+	
 	function devOrientHandler(eventData) {
-		giro = new Array();
-		giro['alpha'] = eventData.alpha;
-	    giro['beta'] = eventData.beta;
-	    giro['gamma'] = eventData.gamma;
-	    console.log(eventData);
-	  	socket.emit('player_move',giro);
+		alpha = eventData.alpha;
+        beta = eventData.beta;
+        gamma = eventData.gamma;
+            
+        if(!mainLoop) {
+        	mainLoop = setInterval(step, 50);    
+        }
 	}
+	
+	function step() {
+		socket.emit('player_move', {
+            a: alpha, 
+            b: beta, 
+            c: gamma,
+        });
+    }
 	        
 	if (window.DeviceOrientationEvent) {
 		console.log('DeviceOrientation is supported');
